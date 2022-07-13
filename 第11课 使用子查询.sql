efa36e90ffac2e12847326f078d8f5e5b5201768
nateshao <shaotongjie@gmail.com>
2022-07-13 23:15:33 +0800

    新增

--- 第11课 使用子查询.sql
+++ 第11课 使用子查询.sql
@@ -37,16 +37,56 @@ SELECT COUNT(*) AS orders
 FROM Orders
 WHERE cust_id = '1000000001';
 
+SELECT cust_name,cust_state,
+ (SELECT COUNT(*) FROM Orders WHERE cust_id = cust_id) AS orders FROM Customers
+ORDER BY cust_name;
+
+SELECT vend_name, prod_name, prod_price
+FROM Vendors, Products
+WHERE Vendors.vend_id = Products.vend_id;
+
+
+SELECT vend_name, prod_name, prod_price FROM Vendors, Products;
+
+SELECT vend_name, prod_name, prod_price FROM Vendors INNER JOIN Products
+ ON Vendors.vend_id = Products.vend_id;
 
 
 
+SELECT prod_name, vend_name, prod_price, quantity
+FROM OrderItems, Products, Vendors
+WHERE Products.vend_id = Vendors.vend_id
+ AND OrderItems.prod_id = Products.prod_id
+ AND order_num = 20007;
 
 
 
+SELECT cust_name, cust_contact FROM Customers, Orders, OrderItems
+WHERE Customers.cust_id = Orders.cust_id AND OrderItems.order_num = Orders.order_num AND prod_id = 'RGAN01';
 
 
+-- 第十四
+SELECT cust_name, cust_contact, cust_email
+FROM Customers
+WHERE cust_state IN ('IL','IN','MI');
 
 
+SELECT cust_name, cust_contact, cust_email
+FROM Customers
+WHERE cust_name = 'Fun4All';
+
+-- 上面两条语句相加
+SELECT cust_name, cust_contact, cust_email FROM Customers WHERE cust_state IN ('IL','IN','MI')
+UNION
+SELECT cust_name, cust_contact, cust_email FROM Customers WHERE cust_name = 'Fun4All';
+
+-- == 
+SELECT cust_name, cust_contact, cust_email FROM Customers WHERE cust_state IN ('IL','IN','MI') OR cust_name = 'Fun4All';
+
+
+SELECT cust_name, cust_contact, cust_email FROM Customers WHERE cust_state IN ('IL','IN','MI')
+UNION ALL
+SELECT cust_name, cust_contact, cust_email FROM Customers WHERE cust_name = 'Fun4All';
 
 
 
